9ce88cae6087d2dfa7df055cc88c8767b2692454
raiden808 <raiden.nalasa@gmail.com>
2019-10-16 05:24:01 +0800

    form submit added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+
+  const [notes,setNotes] = useState(props.notes)
+
+  const addNote = (event) => {
+
+  }
+
   const rows = () => notes.map(note => <Note key={note.id} note={note} />);
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>{rows()}</ul>
+      <form onSubmit={addNote}>
+      	<input />
+      	<button type="submit" >save</button>
+      </form>
     </div>
   );
 };

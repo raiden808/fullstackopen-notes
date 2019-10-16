9030e306aaad61fc4614708bacffa713f4360daf
raiden808 <raiden.nalasa@gmail.com>
2019-10-17 05:34:11 +0800

    add input setting

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import Note from "./components/Note";
 const App = (props) => {
 
   const [notes,setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
 
   const addNote = (event) => {
+    event.preventDefault()
 
+    console.log('button clicked', event.target)
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
   }
 
   const rows = () => notes.map(note => <Note key={note.id} note={note} />);
@@ -16,7 +26,10 @@ const App = (props) => {
       <h1>Notes</h1>
       <ul>{rows()}</ul>
       <form onSubmit={addNote}>
-      	<input />
+      	<input 
+          value={newNote} 
+          onChange={handleNoteChange}
+        />
       	<button type="submit" >save</button>
       </form>
     </div>

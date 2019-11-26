09416c48c9bdf90e7ff39ac3d4ce9bc0c7ba8da9
raiden808 <raiden.nalasa@gmail.com>
2019-11-27 05:58:10 +0800

    fix post request

--- src/App.js
+++ src/App.js
@@ -16,6 +16,14 @@ const App = (props) => {
   }, [])
 
   const toggleImportance = id =>{
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+
     console.log(
       `importance of ${id} needs to be toggled`
     )

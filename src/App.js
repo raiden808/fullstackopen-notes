af8f77bc3ddfbafc870a2321c0012955aca66c34
raiden808 <raiden.nalasa@gmail.com>
2019-11-29 05:52:38 +0800

    refactor notes

--- src/App.js
+++ src/App.js
@@ -9,40 +9,36 @@ const App = (props) => {
   const [showAll,setShowAll] = useState(true)
 
   useEffect(()=>{
-    noteService
-      .getAll()
-      .then(response => {
-        setNotes(response.data)
-      })
+   noteService
+    .getAll()
+    .then(initialNotes => {
+        setNotes(initialNotes)
+    })
   }, [])
 
   const toggleImportance = id =>{
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(note => note.id !== id ? note : response.data))
-    })
-
-    console.log(
-      `importance of ${id} needs to be toggled`
-    )
+    noteService
+      .update(id,changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
   }
 
   const addNote = (event) => {
     event.preventDefault()
-
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
 
